048c5967e68cf517b5f696ced3b8d464ff92a74c
Shivani-techno <shivanichoudharychi@gmail.com>
2021-12-04 23:31:12 +0530

    https://github.com/Shivani-techno/Newsletter_signup.git

---
+++ app.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const https=reuire("https");
+const app=require();
+const bodyParser=require("body-parser");
+const request=require("requesr");
+
+
+
+
+app.listen(3000,function(){
+  console.log("Server is running on port 3000.");
+});
